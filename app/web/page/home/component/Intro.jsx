b86e0fa573ab0327eaa7723cf1b7b67aac39b2a4
ZhangKaitlyn <1873070425@qq.com>
2019-03-15 20:59:15 +0800

    init home page

---
+++ app/web/page/home/component/Intro.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Intro extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="home-title">少形式，多思考</h1>
+        <p className="home-author">
+          蔡小民<span className="seperate">/</span>ui&ux 设计师
+        </p>
+      </div>
+    )
+  }
+}
--- app/web/page/home/home.jsx
+++ app/web/page/home/home.jsx
@@ -1,13 +1,23 @@
-import React, { Component } from 'react';
-import './home.css';
+import React, { Component } from 'react'
+import { Carousel } from 'antd'
+import './home.css'
+import Intro from './component/Intro'
+
 export default class Home extends Component {
   componentDidMount() {
-    console.log('----componentDidMount-----');
+    console.log('----componentDidMount-----')
   }
 
   render() {
-    return <div>
-      <h1>home page</h1>
-    </div>;
+    return (
+      <div className="home-page">
+        <Carousel vertical>
+          <Intro index="1" />
+          <Intro index="2" />
+          <Intro index="3" />
+          <Intro index="4" />
+        </Carousel>
+      </div>
+    )
   }
 }
