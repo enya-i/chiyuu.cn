b2b0d0435fccdddbc0d8103b10849887bfb302f1
ZhangKaitlyn <1873070425@qq.com>
2019-03-27 19:42:15 +0800

    首页固定项

--- app/web/page/home/home.jsx
+++ app/web/page/home/home.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
-import { Carousel } from 'antd'
+import { Carousel, Tooltip } from 'antd'
 import './home.css'
 import Intro from './component/Intro'
 import Works from './component/Works'
 import About from './component/About'
 import Contact from './component/Contact'
+import logoImg from 'asset/images/logo.png'
+import menuImg from 'asset/images/menu.png'
+import QQImg from 'asset/images/QQ.png'
+import wechatImg from 'asset/images/wechat.png'
+import emailImg from 'asset/images/email.png'
+import zcoolImg from 'asset/images/zcool.png'
 
 export default class Home extends Component {
   constructor(props) {
@@ -27,6 +33,22 @@ export default class Home extends Component {
   render() {
     return (
       <div className="home-page" onWheel={this.handleWheelScroll}>
+        <img className="home-page-logo home-page-fixed" src={logoImg} alt="" />
+        <img className="home-page-menu home-page-fixed" src={menuImg} alt="" />
+        <div className="home-page-contacts home-page-fixed">
+          <Tooltip placement="right" title="qq联系方式">
+            <img src={QQImg} alt="" />
+          </Tooltip>
+          <Tooltip placement="right" title="wechat联系方式">
+            <img src={wechatImg} alt="" />
+          </Tooltip>
+          <Tooltip placement="right" title="email联系方式">
+            <img src={emailImg} alt="" />
+          </Tooltip>
+          <Tooltip placement="right" title="zcool联系方式">
+            <img src={zcoolImg} alt="" />
+          </Tooltip>
+        </div>
         <Carousel ref={carousel => (this.carousel = carousel)} vertical>
           <Intro />
           <Works />
